1409655548304278b8ff7087f43ff8445ff8e563
OzielFilho03 <OzielFilho03@hotmail.com>
2020-11-06 00:46:10 -0300

    fix: Name Classe AssetsHelper

--- lib/core/helpers/assetsHelper/AssetsHelper.dart
+++ lib/core/helpers/assetsHelper/AssetsHelper.dart
@@ -1,4 +1,4 @@
-class AssetsflutterHelper {
+class AssetsHelper {
   static String get ashImage => 'assets/images/ash_image.png';
   static String get pokemonImage => 'assets/images/pokemon_image.png';
   
